bed6d7e74327aa3025bf20f0c2e7fc1b18cff22b
Your Name <garralonmourin@gmail.com>
2024-07-24 09:43:45 +0200

    Crear página Places y su ruta

---
+++ src/places/components/PlaceList.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+import Card from "../../shared/components/UIElements/Card";
+import "./PlaceList.css";
+import PlaceItem from "./PlaceItem";
+
+const PlaceList = (props) => {
+  if (props.items.length === 0) {
+    return (
+      <div className="place-list center">
+        <Card>
+          <h2>No places found. Maybe create one?</h2>
+          <button>Share Place</button>
+        </Card>
+      </div>
+    );
+  }
+
+  return (
+    <ul className="place-list">
+      {props.items.map((place) => (
+        <PlaceItem
+          key={place.id}
+          id={place.id}
+          image={place.imageUrl}
+          title={place.title}
+          address={place.address}
+          creator={place.creator}
+          coordinates={place.location}
+        />
+      ))}
+    </ul>
+  );
+};
+
+export default PlaceList;
